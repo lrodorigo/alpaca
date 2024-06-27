9ff99a3c04a254452336243a2ebc00637777908b
Luigi Rodorigo <luigi.rodorigo@angelopo.it>
2024-06-27 13:39:49 +0200

    Small Fix for STM32 compilation

--- include/alpaca/detail/types/temperature.h
+++ include/alpaca/detail/types/temperature.h
@@ -31,10 +31,6 @@ void to_bytes(Container &bytes, std::size_t &byte_index,
 
 }
 
-template <options O, typename T, typename Container>
-void from_bytes_router(T &output, Container &bytes, std::size_t &byte_index,
-                       std::size_t &end_index, std::error_code &error_code);
-
 template <options O, typename Container>
 bool
 from_bytes(Temperature &output, Container &bytes,
